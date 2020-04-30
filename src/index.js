15881c579d76d9af5d4b27464279c2763511f08d
Reto Gmür <reto@factsmission.com>
2020-04-30 13:47:24 +0200

    Added BlankNodeEditor

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export { default as SimpleLiteralEditor } from './valueEditors/SimpleLiteralEditor.svelte';
 export { default as HtmlEditor } from './valueEditors/HtmlEditor.svelte';
+export { default as BlankNodeEditor } from './valueEditors/BlankNodeEditor.svelte';
---
+++ src/valueEditors/BlankNodeEditor.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Factory from "@rdfjs/data-model";
+  export let value;
+  let content;
+  $: content = value.value;
+  function userChange(e) {
+    value = Factory.blankNode(content);
+  }
+</script>
+
+<div>
+  <input type="text" size="5" bind:value={content} on:input={userChange} />
+</div>

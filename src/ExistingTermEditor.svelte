7eb0eb1024b0e5b30b9e5254d792447cce3fcf93
Reto Gmür <reto@factsmission.com>
2020-05-04 20:32:58 +0200

    Added ExistingTermEditor

---
+++ src/ExistingTermEditor.svelte
@@ -0,0 +1,41 @@
+<script>
+  import * as Factory from "@rdfjs/data-model";
+  import HtmlEditor from "./valueEditors/HtmlEditor.svelte";
+  import SimpleLiteralEditor from "./valueEditors/SimpleLiteralEditor.svelte";
+  import NamedNodeEditor from "./valueEditors/NamedNodeEditor.svelte";
+  import BlankNodeEditor from "./valueEditors/BlankNodeEditor.svelte";
+
+  export let value;
+
+  function getType() {
+    const types = {
+      "http://www.w3.org/1999/02/22-rdf-syntax-ns#HTML": "html",
+      "http://www.w3.org/2001/XMLSchema#string": "label",
+      "http://www.w3.org/1999/02/22-rdf-syntax-ns#langString": "langString"
+    };
+    return value.termType === "Literal"
+      ? types[value.datatype.value]
+        ? types[value.datatype.value]
+        : "literal"
+      : value.termType === "NamedNode"
+      ? "IRI"
+      : value.termType === "DefaultGraph"
+      ? "DefaultGraph"
+      : "BlankNode";
+  }
+  const type = getType();
+</script>
+
+<div class="f" :key="type">
+  {#if type === 'html'}
+    <HtmlEditor bind:value on:input />
+  {:else if type === 'IRI'}
+    <NamedNodeEditor bind:value on:input />
+  {:else if type === 'BlankNode'}
+    <BlankNodeEditor bind:value on:input />
+  {:else if type === 'label' || type === 'langString'}
+    <SimpleLiteralEditor bind:value on:input />
+  {:else}
+    <div v-else>No value editor for {type}</div>
+  {/if}
+</div>
--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export { default as SimpleLiteralEditor } from './valueEditors/SimpleLiteralEdit
 export { default as HtmlEditor } from './valueEditors/HtmlEditor.svelte';
 export { default as BlankNodeEditor } from './valueEditors/BlankNodeEditor.svelte';
 export { default as NamedNodeEditor } from './valueEditors/NamedNodeEditor.svelte';
+export { default as ExistingTermEditor } from './ExistingTermEditor.svelte';

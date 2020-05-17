ef6a998b9843fe0ed09f4d1ca9cc529eafe58f70
Reto Gmür <reto@factsmission.com>
2020-05-17 13:19:15 +0200

    Added feature to restore previous value

--- src/TermEditor.svelte
+++ src/TermEditor.svelte
@@ -25,15 +25,47 @@
       )
     : availableOptions;
 
-  function typeSelected(event) {
-    if (type !== null) {
-      value = options[type];
-    }
+  const previousValues = new Map();
+
+  function typeSelected (event) {
+      console.log("this.termTypes: ",termTypes);
+      console.log("typeSelected: ",event);
+      console.log("this.type: ",type);
+      if (!type) {
+          return;
+      }
+      const previousValue = previousValues.get(type);
+      if (previousValue && previousValue.value.length > 0 
+              && window.confirm("Restoring previous value")) { 
+              value = previousValue;
+      } else {
+          console.log("maps to: ",options[(type)]);
+          value = options[type];
+      }
   }
+
   function reset() {
+    previousValues.set(compType(), value);
     type = null;
     value = null;
   }
+
+  function compType () {
+        const types = {
+            "http://www.w3.org/1999/02/22-rdf-syntax-ns#HTML": "html",
+            "http://www.w3.org/2001/XMLSchema#string": "label",
+            "http://www.w3.org/1999/02/22-rdf-syntax-ns#langString": "langString"
+        };
+        return (
+            value !== null ?
+                value.termType === "Literal" ? 
+                    types[value.datatype.value] ?
+                        types[value.datatype.value] : "literal"
+                : value.termType === "NamedNode" ? "IRI" 
+                : value.termType === "DefaultGraph" ? "DefaultGraph"
+                : "BlankNode"
+                : null );
+    }
 </script>
 
 <style>

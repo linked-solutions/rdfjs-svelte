72754140a845ce96fc74de98c0919aa25d56ea2b
Reto Gmür <reto@factsmission.com>
2020-08-02 12:04:54 +0200

    Using cl-editor in a more straight-orward way

--- src/valueEditors/HtmlEditor.svelte
+++ src/valueEditors/HtmlEditor.svelte
@@ -1,39 +1,28 @@
 <script>
   import Factory from "@rdfjs/data-model";
-  import Editor from "cl-editor/dist";
-  import { onMount } from "svelte";
-
-  let editArea;
-  let editor;
+  import Editor from "cl-editor/src/Editor.svelte";
 
   export let value;
 
   let content;
 
   $: {
-    content = value.value;
-    if (editor) {
-      if (editor.getHtml() !== content) {
-        editor.setHtml(content)
-      }
+    if (content !== value.value) {
+      content = value.value;
     }
   }
-  onMount(() => {
-    editor = new Editor({
-      target: editArea,
-      props: {
-        actions: ['b', 'i', 'u', 'strike', 'removeFormat'],
-        height: 'auto',
-        html: content
-      }
-    });
-    editor.$on("change", (e) => {
-      value = Factory.literal(
-        editor.getHtml(),
-        "http://www.w3.org/1999/02/22-rdf-syntax-ns#HTML"
-      );
-    });
-  });
+
+  function contentChanged(e) {
+    value = Factory.literal(
+      editor.getHtml(),
+      "http://www.w3.org/1999/02/22-rdf-syntax-ns#HTML"
+    );
+  }
+
 </script>
 
-<div bind:this={editArea} />
+<Editor
+  actions={['b', 'i', 'u', 'strike', 'removeFormat']}
+  {content}
+  height={"auto"} 
+  on:change={contentChanged} />

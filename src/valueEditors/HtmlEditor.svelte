e6b13f7cac3f7dad216fae5290be2dd638b0452b
Reto Gmür <reto@factsmission.com>
2020-10-20 19:24:29 +0200

    Fixed cl-editor binding

--- src/valueEditors/HtmlEditor.svelte
+++ src/valueEditors/HtmlEditor.svelte
@@ -6,13 +6,19 @@
 
   let content;
 
+  let editor;
+
   $: {
     if (content !== value.value) {
       content = value.value;
+      if (editor) {
+        editor.setHtml(content);
+      }
     }
   }
 
   function contentChanged(e) {
+    content = editor.getHtml()
     value = Factory.literal(
       editor.getHtml(),
       "http://www.w3.org/1999/02/22-rdf-syntax-ns#HTML"
@@ -20,9 +26,8 @@
   }
 
 </script>
-
-<Editor
+<Editor bind:this={editor}
   actions={['b', 'i', 'u', 'strike', 'removeFormat']}
-  {content}
+  html={content}
   height={"auto"} 
   on:change={contentChanged} />
